a504ebef4995df1873a21ddf95f41ba2224225e3
Matheus Serrão <msbb1@icomp.ufam.edu.br>
2021-10-09 21:06:36 -0300

    feat: btcusdt chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AfterViewInit, Component } from '@angular/core';
+import ApexCharts from 'apexcharts';
+import { environment } from 'src/environments/environment';
+import { Klines, parseKline } from './models/klines.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Charts';
+export class AppComponent implements AfterViewInit {
+  constructor(private http: HttpClient) {}
+
+  private options = {
+    series: [
+      {
+        data: [
+          {
+            x: new Date(1538778600000),
+            y: [6629.81, 6650.5, 6623.04, 6633.33],
+          },
+        ],
+      },
+    ],
+    chart: {
+      type: 'candlestick',
+      height: 350,
+    },
+    title: {
+      text: 'CandleStick Chart',
+      align: 'left',
+    },
+    xaxis: {
+      type: 'datetime',
+    },
+    yaxis: {
+      tooltip: {
+        enabled: true,
+      },
+    },
+  };
+
+  ngAfterViewInit() {
+    this.getKlines();
+  }
+
+  getKlines() {
+    const data: {
+      x: Date;
+      y: number[];
+    }[] = [];
+
+    return this.http.get<Klines[]>(environment.url + '/klines').subscribe(
+      (res: Klines[]) => {
+        res.forEach((element: Klines) => {
+          data.push(parseKline(element));
+        });
+        this.options.series[0].data = data;
+        console.log(this.options.series[0].data);
+
+        const chart = new ApexCharts(
+          document.querySelector('#chart'),
+          this.options
+        );
+        chart.render();
+      },
+      () => {},
+      () => {}
+    );
+  }
 }
---
+++ src/app/models/klines.interface.ts
@@ -0,0 +1,30 @@
+export interface Klines {
+  _id: string;
+  symbol: string;
+  timeFrame: string;
+  openTime: string;
+  openPrice: string;
+  highestPrice: string;
+  lowestPrice: string;
+  closePrice: string;
+  volume: string;
+  closeTime: string;
+  quoteAssetVolume: string;
+  numberOfTrades: number;
+  takerBuyBaseAssetVolume: string;
+  takerBuyQuoteAssetVolume: string;
+  trash: string;
+  __v: number;
+}
+
+export function parseKline(kline: Klines) {
+  return {
+    x: new Date(kline.openTime),
+    y: [
+      parseFloat(kline.openPrice),
+      parseFloat(kline.highestPrice),
+      parseFloat(kline.lowestPrice),
+      parseFloat(kline.closePrice),
+    ],
+  };
+}
